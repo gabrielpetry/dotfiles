7c1b4cb6aa3e90d25a5b54ef9265a68d4935c9ab
Gabriel Petry <gabrielpetry@outlook.com>
2019-08-04 20:12:09 -0300

    Improved install_packages.sh

--- packages/install_packages.sh
+++ packages/install_packages.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 
+# Update the pacman base
+sudo pacman -Sy --noconfirm
+
 PACKAGE_LIST_CSV="packages.csv"
 INSTALLED_PACKAGES="$(pacman -Q | awk '{print $1}')"
-
-programsToInstallPacman=""
-programsToInstallAur=""
 
 # Iterate over the programs and create a list of the non installed ones
 while IFS=, read -r install_method program comment; do
     # If already installed, skip
     if echo "${INSTALLED_PACKAGES}" | grep -qo "${program}"; then
+        # echo "${program} [INSTALLED]" 
         continue
     fi
     
     if [[ "${install_method}" =~ (pacman) ]]; then
-        programsToInstallPacman="${programsToInstallPacman} ${program}"
+        echo "Installing ${program} - wait"
+        if pacman -S "${program}" --noconfirm >/dev/null; then
+            echo "${program}(${install_method}) - ${comment} [SUCESS]" 
+        fi
     elif [[ "${install_method}" =~ (aur) ]]; then
-        programsToInstallAur="${programsToInstallAur} ${program}"
+        continue
     fi
-    echo "${program}(${install_method}) - ${comment} está na fila para instalação"
 done < "${PACKAGE_LIST_CSV}"
 
-sudo pacman -Sy --noconfirm
-main() {
-    sudo pacman -S --noconfirm "${programsToInstallPacman}"
-    yay -S --noconfirm "${programsToInstallAur}"
-}
-
-main
