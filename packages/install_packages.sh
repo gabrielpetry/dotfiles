655ba9c7f369cd7a82c9ab8e3576b036254cf280
Gabriel Petry <gabrielpetry@outlook.com>
2019-08-06 15:50:57 -0300

    add reflector to rank mirrors on the start

--- packages/install_packages.sh
+++ packages/install_packages.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
 
 # Update the pacman base
-sudo pacman -Sy --noconfirm
+update_pacman() {
+    # Update the mirrorlist
+    sudo reflector --verbose --country 'Brazil' -l 10 --sort rate --save /etc/pacman.d/mirrorlist
+    sudo pacman -Sy --noconfirm
+}
 
-PACKAGE_LIST_CSV="packages.csv"
-INSTALLED_PACKAGES="$(pacman -Q | awk '{print $1}')"
+install_packages() {
+    PACKAGE_LIST_CSV="packages.csv"
+    INSTALLED_PACKAGES="$(pacman -Q | awk '{print $1}')"
 
-# Iterate over the programs and create a list of the non installed ones
-while IFS=, read -r install_method program comment; do
-    # If already installed, skip
-    if echo "${INSTALLED_PACKAGES}" | grep -qo "${program}"; then
-        # echo "${program} [INSTALLED]" 
-        continue
-    fi
-    
-    if [[ "${install_method}" =~ (pacman) ]]; then
-        echo "Installing ${program} - wait"
-        if pacman -S "${program}" --noconfirm >/dev/null; then
-            echo "${program}(${install_method}) - ${comment} [SUCESS]" 
+    # Iterate over the programs and create a list of the non installed ones
+    while IFS=, read -r install_method program comment; do
+        # If already installed, skip
+        if echo "${INSTALLED_PACKAGES}" | grep -qo "${program}"; then
+            # echo "${program} [INSTALLED]" 
+            continue
         fi
-    elif [[ "${install_method}" =~ (aur) ]]; then
-        continue
-    fi
-done < "${PACKAGE_LIST_CSV}"
+        
+        if [[ "${install_method}" =~ (pacman) ]]; then
+            echo "Installing ${program} - wait"
+            if pacman -S "${program}" --noconfirm >/dev/null; then
+                echo "${program}(${install_method}) - ${comment} [SUCESS]" 
+            fi
+        elif [[ "${install_method}" =~ (aur) ]]; then
+            continue
+        fi
+    done < "${PACKAGE_LIST_CSV}"
 
+}
+
+main() {
+    update_pacman && \
+    install_packages
+}
+
+
+main
