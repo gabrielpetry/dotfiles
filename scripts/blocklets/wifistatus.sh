36ef7f661ca6f4b7e5b19eea97545e03f789cc75
Gabriel Petry <gabrielpetry@outlook.com>
2019-08-12 14:03:35 -0300

    Added info when connected to cabble

--- scripts/blocklets/wifistatus.sh
+++ scripts/blocklets/wifistatus.sh
@@ -3,6 +3,7 @@
 LABEL=""
 
 color=""
+wifi_connection_name="No connection"
 wifi_card_name="$(ip a | grep -Eo "wlp[0-9]s[0-9]" | tail -n 1)"
 
 wifi_info="$(iwconfig "${wifi_card_name}")"
@@ -18,6 +19,15 @@ if [[ "$(echo "${wifi_quality}" | cut -d '/' -f1 )" -lt "51" ]]; then
     background_color="#$(grep "color12" "${HOME}/.Xresources" | \
 		      cut -d "#" -f2)"
     color="color='${background_color}'"
+fi
+
+if [[ -z "$wifi_connection_name" ]]; then
+    cabble_status="$(cat '/sys/class/net/enp1s0/operstate')" 
+    LABEL=""
+    wifi_connection_name="No connection";
+    if [[ "$cabble_status" == 'up' ]]; then
+	wifi_connection_name="Cable/DSL"
+    fi
 fi
 
 printf "<span %s>%s %s %s</span>" \
