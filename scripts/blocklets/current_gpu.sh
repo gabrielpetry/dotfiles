f909c44124ed707d2456ce480466a1708ea78e0b
Gabriel Petry <gabrielpetry@outlook.com>
2019-10-06 10:16:24 -0300

    Show which gpu is currently in use

---
+++ scripts/blocklets/current_gpu.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+clear
+
+optimus_manager="$(command -v optimus-manager)"
+
+if [ -z "$optimus_manager" ]; then
+    exit
+fi
+
+# optimus-manager --print-mode
+status="$($optimus_manager --print-mode | \
+            cut -d':' -f2 | \
+            tr -d '\n ')"
+
+color="purple"
+color="$("$SCRIPTS_DIR"/getColor.sh red)"
+if [ "$status" = "nvidia" ]; then
+    color="$("$SCRIPTS_DIR"/getColor.sh magenta)"
+fi
+
+printf "<span color='%s'>%s</span>\n" \
+    "$color" \
+    "$status"
