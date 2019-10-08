5a3bda6bd00fb2e3f2e7df92acf28a42031231fd
Gabriel Petry <gabrielpetry@outlook.com>
2019-10-08 09:43:06 -0300

    Added a script to get bitcoin price

---
+++ scripts/blocklets/bitcoin.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+last_sell="$(curl -Ss https://www.mercadobitcoin.net/api/BTC/ticker)"
+
+
+value="$(echo "$last_sell" | jq ".ticker.sell" | tr -d '\"' | cut -d"." -f1)"
+
+
+color="#e2e2e2"
+if [[ "$value" -gt "40000" ]]; then
+    color="$("$SCRIPTS_DIR"/getColor.sh magenta)"
+fi
+printf "<span color='%s'>R$ %s</span>\n" \
+    "$color" \
+    "$value"
+ 
