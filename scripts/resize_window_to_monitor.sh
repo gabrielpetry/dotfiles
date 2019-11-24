c7b0e1a064ef5ec3fd93072dcc88cf8f99ea354c
Gabriel Petry <gabrielpetry@outlook.com>
2019-11-24 09:22:51 -0300

    move to a standalone script

--- scripts/resize_window_to_monitor.sh
+++ scripts/resize_window_to_monitor.sh
@@ -7,6 +7,9 @@
 #     tail -n 2 | head -n 1))
 # screen3=($(xrandr | grep -w connected  | awk -F'[ +]' '{print $1,$3,$4}' | 
 #     head -n 1))
+createLog() {
+    #  echo "$(date) $@" >> /tmp/resizeWindow.log
+}
 
 screens="$(xrandr | \
            grep -w connected | \
@@ -14,22 +17,20 @@ screens="$(xrandr | \
            awk -F'[ +]' '{print $1, $3, $4}' | \
            sort --key 3 --numeric-sort)"
 
-currentWindowPosition=$(xwininfo -id $(xdotool getactivewindow) | \
-                grep "Absolute upper-left X" | \
-                awk '{print $NF}')
-
+eval $(xdotool getmouselocation --shell)
+currentMousePosition=$X
 monitorsOcupied=""
 
 while read monitor; do
 monitorRight="$(echo "$monitor" | awk '{print $3}' )"
-    if [ "$currentWindowPosition" -gt "$monitorRight" ]; then
+    if [ "$currentMousePosition" -gt "$monitorRight" ]; then
         monitorsOcupied="$monitorsOcupied
         $monitor"
     fi
 done <<< "$screens"
 
-echo "screens: " "$screens"
-echo "monitors ocupied: " "$monitorsOcupied"
+createLog "screens: " "$screens"
+createLog "monitors ocupied: " "$monitorsOcupied"
 
 if [ "$monitorsOcupied" != "" ]; then
     monitorWithTheWindow="$(echo "$monitorsOcupied" | tail -n 1)"
@@ -37,45 +38,25 @@ else
     monitorWithTheWindow="$(echo "$screens" | head -n 1)"
 fi
 
+i3-msg scratchpad show
 
-
-echo "Monitor with the window: $monitorWithTheWindow"
+createLog "Monitor with the window: $monitorWithTheWindow"
 
 resolutions="$(echo $monitorWithTheWindow | awk '{print $2}')"
+createLog "Resolutions $resolutions"
 resolutionX="$(echo $resolutions | cut -dx -f1)"
 resolutionY="$(echo $resolutions | cut -dx -f2)"
 
-if [  ]
-i3-msg resize set $(expr $resolutionX - 100) $(expr $resolutionY - 100) && \
-i3-msg move position center
+scratchPadIsVisible="$(wmctrl -lp | grep -E "\0\x[0-9a-z]+?\s+-1")"
+
+if [ -z "$scratchPadIsVisible" ]; then
+    createLog "scratchpad status $scratchPadIsVisible"
+    i3-msg resize set $(expr $resolutionX - 100) $(expr $resolutionY - 100) && \
+    i3-msg move position center
+ fi
+
+
+createLog "====== EOE ======"
 
 exit
 
-
-## Figure out which screen is to the right of which
-# if [ ${screen1[2]} -eq 0  ]
-# then
-#     right=(${screen2[@]});
-#     left=(${screen1[@]});
-# else
-#     right=(${screen1[@]});
-#     left=(${screen2[@]});
-# fi
-
-## Get window position
-pos=$(xwininfo -id $(xdotool getactivewindow) | grep "Absolute upper-left X" | 
-      awk '{print $NF}')
-
-## Which screen is this window displayed in? If $pos
-## is greater than the offset of the rightmost screen,
-## then the window is on the right hand one
-current_monitor=""
-
-if [ "$pos" -gt "${right" ]
-
-# if [ "$pos" -gt "${right[2]}" ]
-# then
-#     echo "${right[0]} : ${right[1]}"    
-# else
-#     echo "${left[0]} : ${left[1]}"    
-# fi
