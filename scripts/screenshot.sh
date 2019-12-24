36336ef877e8cc5c65fa41d812de149212b34350
Gabriel Petry <gabrielpetry@outlook.com>
2019-12-24 18:32:29 -0300

    create print screen folder if not exists

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -3,6 +3,10 @@
 options="ScrotToGimp\nScrotToClipboard\nJustSave"
 path="$HOME/Imagens/Print"
 
+if [[ ! -d "$path" ]]; then
+	mkdir -p "$path"
+fi
+
 filename=$(scrot -s -z "$path/%Y-%m-%d_temp_name.png" -e 'echo $f')
 
 chosen=$(printf "%b" "$options" | dmenu -i )
