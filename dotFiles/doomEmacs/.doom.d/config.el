1885409e3485ed05cce7fb05aa23773a674e57b0
Gabriel Petry <gabrielpetry@outlook.com>
2019-12-31 07:58:33 -0300

    change font in emacs

--- dotFiles/doomEmacs/.doom.d/config.el
+++ dotFiles/doomEmacs/.doom.d/config.el
@@ -19,8 +19,12 @@
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
 ;; test
-(setq doom-font (font-spec :family "Cascadia Code" :size 14)
-      doom-variable-pitch-font (font-spec :family "sans"))
+
+(set-face-attribute 'default nil
+                    :family "Cascadia Code Regular"
+                    :height 100
+                    :weight 'normal
+                    :width 'normal)
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
