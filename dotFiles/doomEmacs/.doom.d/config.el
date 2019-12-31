0ccb35f65a30c96a0636fff77192b18b2e4ae91e
Gabriel Petry <gabrielpetry@outlook.com>
2019-12-31 09:20:20 -0300

    emacs show current function in the lower bar

--- dotFiles/doomEmacs/.doom.d/config.el
+++ dotFiles/doomEmacs/.doom.d/config.el
@@ -58,3 +58,13 @@
 
 (setq projectile-project-search-path '("~/Projetos"))
 (setq line-spacing 0.4)
+
+
+;; Show the current function name in the header line
+(which-function-mode 1)
+(setq-default header-line-format
+              '((which-func-mode ("" which-func-format " "))))
+(setq mode-line-misc-info
+            ;; We remove Which Function Mode from the mode line, because it's mostly
+            ;; invisible here anyway.
+            (assq-delete-all 'which-func-mode mode-line-misc-info))
