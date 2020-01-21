009807ac13ec92544d588ccee1ca7dc4a3822e60
Gabriel Petry <gabriel.petry@kinghost.com.br>
2020-01-21 09:36:49 -0300

    addded line spacing

--- dotFiles/doomEmacs/.doom.d/packages.el
+++ dotFiles/doomEmacs/.doom.d/packages.el
@@ -41,6 +41,3 @@
 ;; This is required for some packages whose default branch isn't 'master' (which
 ;; our package manager can't deal with; see raxod502/straight.el#279)
 ;(package! builtin-package :recipe (:branch "develop"))
-
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
