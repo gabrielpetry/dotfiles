091f4c757eda661ecd1170b2ff3560acf73809f6
Gabriel Petry <gabriel.petry@kinghost.com.br>
2020-03-31 14:29:23 -0300

    color pattern

--- scripts/blocklets/vpn_status.sh
+++ scripts/blocklets/vpn_status.sh
@@ -3,8 +3,7 @@
 
 color='white'
 
-[ -n "$(pgrep openvpn && echo 1)" ] && color='green'
-
+[ -n "$(pgrep openvpn && echo 1)" ] && color="$($SCRIPTS_DIR/getColor.sh magenta)"
 printf "<span color='%s'>%s</span>\n" \
     "${color}" \
     " "
