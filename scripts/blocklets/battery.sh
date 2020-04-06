e3ebab777832b59ea6dbac128be055b17774b6a8
Gabriel Petry <gabriel.petry@kinghost.com.br>
2020-04-06 20:03:01 -0300

    get all bateries

--- scripts/blocklets/battery.sh
+++ scripts/blocklets/battery.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-level="$(cat /sys/class/power_supply/BAT1/capacity)"
+level="$(cat /sys/class/power_supply/BAT?/capacity)"
 
 
 color="$PRIMARY_COLOR"
 
-[ "$level" -lt "60" ] && color="$ALERT_COLOR"
-[ "$level" -lt "20" ] && color="$DANGER_COLOR"
+[[ "$level" -lt "60" ]] && color="$ALERT_COLOR"
+[[ "$level" -lt "20" ]] && color="$DANGER_COLOR"
 
-printf "<span color='%s'>%s %%</span>" \
+printf "<span color='%s'>%s %%</span>\n" \
     "$color" \
     "$level"
 
