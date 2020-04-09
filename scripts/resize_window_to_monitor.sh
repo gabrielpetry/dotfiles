e2adcce156676af586742f8afe8cb90dad31145b
Gabriel Petry <gabrielpetry@gabrielpetry.com.br>
2020-04-09 15:33:09 -0300

    fixed bug when showing

--- scripts/resize_window_to_monitor.sh
+++ scripts/resize_window_to_monitor.sh
@@ -51,16 +51,17 @@ resolutionY="$(echo $resolutions | cut -dx -f2)"
 
 scratchPadIsVisible="$(wmctrl -lp | grep -E "\0\x[0-9a-z]+?\s+-1")"
 
-# if [ -z "$scratchPadIsVisible" ]; then
+i3-msg scratchpad show
+
+if [ -n "$scratchPadIsVisible" ]; then
     createLog "scratchpad status $scratchPadIsVisible"
     if [[ "$resolutionX" -gt "1920" ]]; then 
         resolutionX=1920
     fi
-    i3-msg scratchpad show
     i3-msg move position pointer
     i3-msg resize set $(expr $resolutionX - 100) $(expr $resolutionY - 100) && \
     i3-msg move position center
- # fi
+fi
 
 
 createLog "====== EOE ======"
