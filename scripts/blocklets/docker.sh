fff51acfb00b338f8df9a10c89927af79bea8f6b
Gabriel Petry <gabrielpetry@gabrielpetry.com.br>
2020-04-15 11:50:06 -0300

    fix docker script

--- scripts/blocklets/docker.sh
+++ scripts/blocklets/docker.sh
@@ -6,7 +6,7 @@ color="$PRIMARY_COLOR"
 
 docker_count="$(docker ps -q 2>/dev/null | wc -l)"
 
-docker ps -q 2> /dev/null || docker_count=-1
+docker ps -q > /dev/null || docker_count=-1
 
 [ "$docker_count" -gt "5" ] && color="$ALERT_COLOR"
 [[ "$docker_count" = "-1" || "$docker_count" -gt "7" ]] && color="$DANGER_COLOR"
