26fa3250f77d72554c287f6e83140fd83db1f14f
Gabriel Petry <gabriel.petry@kinghost.com.br>
2020-04-22 10:42:26 -0300

    fix docker

--- scripts/blocklets/docker.sh
+++ scripts/blocklets/docker.sh
@@ -6,12 +6,12 @@ color="$PRIMARY_COLOR"
 
 docker_count="$(docker ps -q 2>/dev/null | wc -l)"
 
+
+
 docker ps -q > /dev/null || docker_count=-1
 
 [ "$docker_count" -gt "5" ] && color="$ALERT_COLOR"
 [[ "$docker_count" = "-1" || "$docker_count" -gt "7" ]] && color="$DANGER_COLOR"
 
-printf "<span color='%s'> %s</span>" \
-    "$color" \
-    "$docker_count"
+echo "<span color='$color'>  $docker_count</span>"
 
