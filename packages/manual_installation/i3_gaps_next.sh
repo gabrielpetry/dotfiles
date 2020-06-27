bdd2ce946fd8d6fa489601afa40f2a5161bd63a7
Gabriel Petry <contato@gabrielpetry.com.br>
2020-06-27 14:26:48 -0300

    i3 gaps next fedora

--- packages/manual_installation/i3_gaps_next.sh
+++ packages/manual_installation/i3_gaps_next.sh
@@ -11,8 +11,8 @@ CPU_CORES="$(expr $(grep processor /proc/cpuinfo | wc -l) + 1)"
 git clone -b gaps-next https://www.github.com/Airblader/i3 i3-gaps
 cd i3-gaps
 
-
-sudo apt install git libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev -y
+# sudo apt install git libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev -y
+sudo dnf install -y libxcb-devel xcb-util-keysyms-devel xcb-util-devel xcb-util-wm-devel xcb-util-xrm-devel yajl-devel libXrandr-devel startup-notification-devel libev-devel xcb-util-cursor-devel libXinerama-devel libxkbcommon-devel libxkbcommon-x11-devel pcre-devel pango-devel git gcc automake i3status i3lock make
 
 # compile & install
 autoreconf --force --install
