33c3b4e6906f1967e2b2d33f0e3fbcafa6c01e77
Gabriel Petry <contato@gabrielpetry.com.br>
2020-06-27 14:32:04 -0300

    exception to fedora

--- packages/manual_installation/docker.sh
+++ packages/manual_installation/docker.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+release="$(cat /etc/*-release | grep 'NAME')"
+
+
+if echo $release | grep -iq 'fedora'; then
+    echo Fedora does not like default docker
+    echo "https://fedoramagazine.org/docker-and-fedora-32/"
+    exit 
+fi
+
+
+
 
 curl -Ss https://get.docker.com | bash -
