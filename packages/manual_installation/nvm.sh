cdd56a5017a6d0a60f48a0c5c7eabc52a0de3e9f
Gabriel Petry <gabrielpetry@gabrielpetry.com.br>
2020-10-09 20:05:34 -0300

    install latest node

--- packages/manual_installation/nvm.sh
+++ packages/manual_installation/nvm.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 curl -Ss -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+source ~/.bashrc
+# v12.18.3   (LTS: Erbium)
+# v12.18.4   (LTS: Erbium)
+# v12.19.0   (Latest LTS: Erbium)
+nvm install $(nvm ls-remote | grep -i lts | tail -n 1 | cut -d'(' -f 1)
