5b8faacf0498dc7543cafa54e8cdca1105e5598f
Gabriel Petry <gabrielpetry@gabrielpetry.com.br>
2020-10-10 08:31:34 -0300

    install latest node

--- packages/manual_installation/nvm.sh
+++ packages/manual_installation/nvm.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
-command -v nvm > /dev/null %% exit 0
 curl -Ss -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
 source ~/.bashrc
 # v12.18.3   (LTS: Erbium)
 # v12.18.4   (LTS: Erbium)
 # v12.19.0   (Latest LTS: Erbium)
-nvm install $(nvm ls-remote | grep -i lts | tail -n 1 | cut -d'(' -f 1)
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install $(nvm ls-remote | grep -i lts | tail -n 1 | cut -d' ' -f 2)
